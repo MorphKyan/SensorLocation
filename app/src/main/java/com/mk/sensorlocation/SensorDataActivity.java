19ef48292850c94a5d0d1770820ccaa0063580a3
morph <morphkyan@gmail.com>
2020-11-06 17:19:56 +0800

    Initial commit

---
+++ app/src/main/java/com/mk/sensorlocation/SensorDataActivity.java
@@ -0,0 +1,14 @@
+package com.mk.sensorlocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SensorDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_data);
+    }
+}
